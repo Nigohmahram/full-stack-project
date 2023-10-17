d84cee03c46f9764be4aef503f6c269dc7563aae
Usmonov Hojiakbar <usmonovmaxsudovich@gmail.com>
2023-10-17 23:27:27 +0900

    Article slice &&  Card

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import AuthService from './service/auth';
 import { useDispatch } from 'react-redux';
 import { signUserSuccess } from './slice/auth';
 import { getItem } from './helpers/persistance-storage';
+import ArticleService from './service/article';
+import { getArticlesStart, getArticlesSuccess } from './slice/article';
 
 const App = () => {
 	const dispatch = useDispatch();
@@ -15,11 +17,21 @@ const App = () => {
 		} catch (error) {}
 		console.log('error');
 	};
+	const getArticles = async () => {
+		dispatch(getArticlesStart);
+		try {
+			const response = await ArticleService.getArticles();
+			dispatch(getArticlesSuccess(response.articles));
+		} catch (error) {
+			console.log(error);
+		}
+	};
 	useEffect(() => {
 		const token = getItem('token');
 		if (token) {
 			getUser();
 		}
+		getArticles();
 	}, []);
 	return (
 		<div>
